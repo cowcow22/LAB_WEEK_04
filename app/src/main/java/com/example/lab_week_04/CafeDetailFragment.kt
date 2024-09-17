8c824c409d4796e299500d2e89a2aa4ffa0c4c3c
cowcow1 <stevanusfirman22@gmail.com>
2024-09-17 17:36:21 +0700

    Update lagi

--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -7,45 +7,34 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.widget.TextView
 
+private const val TAB_CONTENT = "TAB_CONTENT"
 class CafeDetailFragment : Fragment() {
-
-    companion object {
-        private const val ARG_TITLE = "title"
-        private const val ARG_DESCRIPTION = "description"
-
-        // Create a new instance of CafeDetailFragment and pass the arguments
-        fun newInstance(titleResId: Int, descriptionResId: Int): CafeDetailFragment {
-            val fragment = CafeDetailFragment()
-            val args = Bundle()
-            args.putInt(ARG_TITLE, titleResId)
-            args.putInt(ARG_DESCRIPTION, descriptionResId)
-            fragment.arguments = args
-            return fragment
+    private var content: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            content = it.getString(TAB_CONTENT)
         }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_cafe_detail, container, false)
-
-        val titleTextView: TextView = view.findViewById(R.id.cafe_title)
-        val descriptionTextView: TextView = view.findViewById(R.id.cafe_description)
-
-        // Retrieve arguments passed to the fragment
-        val titleResId = arguments?.getInt(ARG_TITLE)
-        val descriptionResId = arguments?.getInt(ARG_DESCRIPTION)
-
-        // Set the title and description
-        if (titleResId != null) {
-            titleTextView.setText(titleResId)
-        }
-        if (descriptionResId != null) {
-            descriptionTextView.setText(descriptionResId)
-        }
-
-        return view
+// Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.content_description)
+            ?.text = content
+    }
+    companion object {
+        fun newInstance(content: String) =
+            CafeDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(TAB_CONTENT, content)
+                }
+            }
     }
 }
+
